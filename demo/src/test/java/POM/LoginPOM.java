a13776014c982ceda3bf700ddc8ed541d71799bb
Usaamah Kriel <usaamah.kriel@acumensoft.net>
2024-02-27 10:22:02 +0200

    created login POM

---
+++ demo/src/test/java/POM/LoginPOM.java
@@ -0,0 +1,33 @@
+package POM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+/**
+ * LoginPOM
+ */
+public class LoginPOM {
+
+    private String usernameID = "user-name";
+    private String passwordID = "password";
+    private String loginButtonID = "login-button";
+    private WebDriver driver;
+    public static String loginURL = "https://qa-challenge.codesubmit.io";
+
+    public LoginPOM(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void typeUsername(String username){
+        driver.findElement(By.id(usernameID)).sendKeys(username);
+    }
+
+    public void typePassword(String password){
+        driver.findElement(By.id(passwordID)).sendKeys(password);
+    }
+
+    public void clickLogin(){
+        driver.findElement(By.id(loginButtonID)).click();
+    }
+}
